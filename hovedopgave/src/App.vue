3a0761bdf5dfcd9bf71317ca94d9bf44624ed85e
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-12-07 15:07:29 +0100

    begun making footer

--- hovedopgave/src/App.vue
+++ hovedopgave/src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import Nav from './components/header/Navbar.vue'
+import Nav from './components/header/Navbar.vue';
+import Footer from './components/footer/Footer.vue';
+
 </script>
 
 <template>
     <div>
 
         <Nav></Nav>
+
+        
         
         <RouterView></RouterView>
+
+        <Footer></Footer>
         
     </div>
 </template>
---
+++ hovedopgave/src/components/footer/Footer.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+ <main>
+    <footer class="footer">
+        <section class="footer-top">
+            <div class="footer-top-section">
+                <h2 class="footer-top-heading">Support</h2>
+
+            </div>
+
+        </section>
+
+    </footer>
+    
+  </main>
+  
+</template>
+
+<style lang="scss" scoped>
+@import "../footer/Footer.scss";
+@import "../../assets/styling/variables/colors";
+
+</style>
