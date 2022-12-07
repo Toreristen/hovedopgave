99d8ec558ca245870650f1281e4c048e83c4f19f
KasperPtak <kasperptak2@gmail.com>
2022-12-07 11:30:18 +0100

    updated index.js so views are imported
    Co-Authored-By: Toreristen <82028481+Toreristen@users.noreply.github.com>

--- hovedopgave/src/router/index.js
+++ hovedopgave/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import CasesView from '../views/CasesView.vue'
+import ContactView from '../views/ContactView.vue'
+import FaggrupperView from '../views/FaggrupperView.vue'
+import YdelserView from '../views/YdelserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +15,12 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/om',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/kontakt',
