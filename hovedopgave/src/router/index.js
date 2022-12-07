e3ccf8d691ae382815682ac1c44cb51d3f25c1c7
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-12-07 11:16:19 +0100

    pages ready and router setup

--- hovedopgave/src/router/index.js
+++ hovedopgave/src/router/index.js
@@ -16,6 +16,26 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/kontakt',
+      name: 'kontakt',
+      component: ContactView
+    },
+    {
+      path: '/cases',
+      name: 'cases',
+      component: CasesView
+    },
+    {
+      path:'/faggrupper',
+      name: 'faggrupper',
+      component: FaggrupperView
+    },
+    {
+      path: '/ydelser',
+      name: 'ydelser',
+      component: YdelserView
     }
      
   ]
