e1a4f66954f362cf4dbfb3e0fe597b92ec1cf3d5
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-12-08 10:24:07 +0100

    Footer layout finished, begun styling

--- hovedopgave/src/components/footer/Footer.vue
+++ hovedopgave/src/components/footer/Footer.vue
@@ -8,6 +8,29 @@
         <section class="footer-top">
             <div class="footer-top-section">
                 <h2 class="footer-top-heading">Support</h2>
+                <p class="footer-top-txt">Står du og har brug for hjælp med et af vores programmer? 
+                Så kan du få hjælp og vejledning på supportsiden ved at klikke her</p>
+                <a class="footer-top-btn" href="#">Gå til support</a>             
+            </div>
+            <div class="footer-top-section">
+                <h2 class="footer-top-heading">Find os</h2>
+                <p class="footer-top-txt">Du har altid mulighed for at kontakte os. Om det er stort eller småt, så står vi altid til rådighed </p>
+                
+            </div>
+            <div class="footer-top-section">
+                <h2 class="footer-top-heading">Kontakt os</h2>
+                <p class="footer-top-txt">Du har altid mulighed for at kontakte os. Om det er stort eller småt, så står vi altid til rådighed </p>
+
+            </div>
+
+        </section>
+        <section class="footer-bottom">
+            <div class="footer-bottom-left">
+                <h2 class="footer-heading">bottom left</h2>
+
+            </div>
+            <div class="footer-bottom-right">
+                <h2 class="footer-heading">bottom right</h2>
 
             </div>
 
