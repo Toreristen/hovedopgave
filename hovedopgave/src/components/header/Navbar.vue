732028d61fb50f2ddaf2480fb272e3c93c41e8bf
KasperPtak <kasperptak2@gmail.com>
2022-12-08 16:34:33 +0100

    dropdown/modal for nav functionality

--- hovedopgave/src/components/header/Navbar.vue
+++ hovedopgave/src/components/header/Navbar.vue
@@ -1,46 +1,58 @@
-<script setup>
+<script>
+
 import { RouterLink } from "vue-router";
+
+export default {
+  name: "ToggleDiv",
+  data: function () {
+    return {
+      isShow: false,
+    };
+  },
+
+ 
+};
+
 </script>
 
 <template>
   <div class="bg">
-
     <header class="main-header">
       <div class="header-logo">
-        <img class="img-fluid" src="../../assets/img/logo_2022_geodata_orange.svg" alt="Logo" />
+        <img
+          class="img-fluid"
+          src="../../assets/img/logo_2022_geodata_orange.svg"
+          alt="Logo"
+        />
       </div>
       <nav class="header-nav">
         <RouterLink class="header-links" to="/">Home</RouterLink>
-        <RouterLink class="header-links" to="/ydelser">Ydelser</RouterLink>
-        <RouterLink class="header-links" to="/faggrupper">Faggrupper</RouterLink>
+        <div @click="isShow = !isShow" id="ydelser" class="header-links dropdown">
+          Ydelser ^
+          <div v-show="isShow" class="dropdown-content">
+            <div class="grid-container">
+              <p>
+                Dropdown indhold her
+              </p>
+              <RouterLink to="/ydelser">
+                Gå til Ydelser
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+        <RouterLink class="header-links" to="/faggrupper"
+          >Faggrupper</RouterLink
+        >
         <RouterLink class="header-links" to="/cases">Cases</RouterLink>
         <RouterLink class="header-links" to="/om-os">Om os</RouterLink>
         <RouterLink class="header-links" to="/kontakt">Kontakt</RouterLink>
       </nav>
       <div class="header-support">
-        <a href="#">
-          support
-        </a>
+        <a href="#"> support </a>
       </div>
     </header>
 
-    <div class="dropdown">
-    <button class="dropbtn">
-      Ydelser 
-      <!-- pil ned her, fontawesome prob -->
-    </button>
-    <div class="dropdown-content">
-
-      <div class="grid-container">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        <a href="#">Link 3</a>
-
-      </div>
-
-    </div>
-  </div> 
-
+    
   </div>
 </template>
 <!-- Make it scoped to not make it apply to everything -->
