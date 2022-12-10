be7bd766cbe17eac48ee7ec3ec7329674316fc2d
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-12-10 23:01:10 +0100

    small fixes and changes to components

--- hovedopgave/src/components/frontpage/CasesCTA.vue
+++ hovedopgave/src/components/frontpage/CasesCTA.vue
@@ -6,7 +6,7 @@
     <section class="cases">
         <div class="cases-about">
             <h2 class="cases-heading">Læs hvordan Brønderslev kommune formindskede boligpresset i midtbyen</h2>
-            <p class="cases-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ut molestias debitis voluptates, similique fugit officiis, iusto libero est odit quae explicabo corporis non. Autem ex reprehenderit natus ab nihil ipsam facere molestiae iure, in dolore neque ratione hic mollitia!</p>
+            <p class="cases-txt">Brønderslev kommune har i de sidste fire år oplevet en stærkt stigende vækst af tilflyttere, som har fundet drømmehjemmet i den dejlige by i Nordjylland. De nye tilflyttere søger oftest ind mod centrum af byen, og med flere indbyggere har det skabt et større behov for at opføre nye lejlighedskomplekser. Dette har Brønderslev kunnet komme i forkøb med, ved at gøre brug af Census’ befolkningsprognose. Læs her mere om hvilke fordele Brønderslev kommune har ved at bruge Census.</p>
             <a href="#" class="cases-btn">Læs mere</a>
         </div>
         <img src="../../assets/img/cases-desktop.png" alt="" class="cases-img img-fluid">
--- hovedopgave/src/components/frontpage/Intro.vue
+++ hovedopgave/src/components/frontpage/Intro.vue
@@ -6,7 +6,9 @@
 
         <section class="child-1">
             <h2 class="heading">Skab et overblik med Geodata</h2>
-            <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum quod ex voluptatem nesciunt, debitis reprehenderit dolor facilis autem maxime ducimus. Tenetur, debitis vero pariatur repellat minima aperiam eos ea quo placeat maxime at impedit rerum fugit delectus dolorem autem!</p>
+            <p class="txt">I Geodata har vi specialiseret os i at kunne forsyne vores kunder med data fra en lang række forskellige datakilder.  Adgangen til gode og valide data giver bl.a. mulighed for at optimere arbejdsprocesser, skabe grobund for vækst samt udvikle nye forretningsområder.
+                <br><br>
+            LIFA-Geodata har opbygget en serie af generelle og kundespecifikke dataydelser, som anvendes i vidt forskellige forretningsområder – lige fra detailhandel til porteføljestyring og offentlig forvaltning.</p>
 
             <div class="btn">
                 <a href="#" class="read-more">Læs mere </a>
@@ -21,8 +23,11 @@
             <img src="../../assets/img/cases-desktop.png" alt="" class="img-fluid">
         </section>
         <section class="child-4">
-            <h2 class="heading">Skab et overblik med Geodata</h2>
-            <p class="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum quod ex voluptatem nesciunt, debitis reprehenderit dolor facilis autem maxime ducimus. Tenetur, debitis vero pariatur repellat minima aperiam eos ea quo placeat maxime at impedit rerum fugit delectus dolorem autem!</p>
+            <h2 class="heading">Geodata er en del af LIFA familien</h2>
+            <p class="txt">Geodata er en afdeling af LIFA A/S, som der gennem årene har udviklet den traditionelle landinspektørforretning til også at omfatte specialistviden på GIS-, OIS- og CAD-området.
+            <br><br>
+            Udgangspunktet for firmaets forretningsområde er de traditionelle landinspektøropgaver, der med tiden har udviklet sig til også at omfatte distribution af landsdækkende ejendomsdata samt udvikling af CAD- og GIS-applikationer til håndtering af ejendomsdata på markedets dominerende CAD- og GIS-platforme.
+            </p>
 
             <div class="btn">
                 <a href="#" class="read-more">Læs mere </a>
