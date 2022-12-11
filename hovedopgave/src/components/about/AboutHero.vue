879b65297dba661d6d3a69b4b4765fd9e02389f6
Toreristen <tore.machete@hotmail.com>
2022-12-11 16:04:12 +0100

    about banner desktop ready

--- hovedopgave/src/components/about/AboutHero.vue
+++ hovedopgave/src/components/about/AboutHero.vue
@@ -5,10 +5,11 @@
 <template>
     <section class="container">
         <div class="txt-square">
-            stuff
+            <h1 class="main-header">Om Geodata</h1>
+            <p class="txt">Læs mere om hvem vi er i Geodata, og hvordan vi i de sidste 10 år har leveret avanceret GIS, OIS og CAD løsninger. </p>
+            <a href="#" class="btn">Kontakt os</a>
         </div>
         <div class="block-square">
-            chops
         </div>
     </section>
 </template>
