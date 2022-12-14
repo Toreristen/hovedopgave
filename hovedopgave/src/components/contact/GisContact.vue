fd751a490134bb69fd754330ff78b0983fe461da
Toreristen <tore.machete@hotmail.com>
2022-12-14 10:54:19 +0100

    added content to giscontact and made  loiscontact responsive

--- hovedopgave/src/components/contact/GisContact.vue
+++ hovedopgave/src/components/contact/GisContact.vue
@@ -3,11 +3,44 @@
 </script>
 
 <template>
-<h1>
-    Gis
-</h1>
+
+<article class="lois">
+    <section class="lois-txt">
+        <h3 class="sub-header">Kontakt og support</h3>
+        <h2 class="main-header">GIS</h2>
+        <p class="info-txt">Du har mulighed for at tage kontakt til Allan eller Henrik eller besøge Geodatas supportportal, hvor du kan finde guides og tutorials til brug af både Census og LOIS programmerne.    
+        </p>
+        <div class="btn">
+            <a href="#" class="btn-a">Gå til support</a>
+        </div>
+    </section>
+
+    <section class="contact-portraits">
+        <div class="portrait-container">
+            <img class="img" src="../../assets/img/henrik_pb.jpg" alt="Henrik">
+            <div class="image-overlay">
+                <p class="title">Landinspektør, Partner</p>
+                <h3 class="name">Henrik Skov Uldall</h3>
+                <p class="contact-info"><font-awesome-icon icon="fa-solid fa-phone" /> +45 63 13 68 56</p>
+                <p class="contact-info"><font-awesome-icon icon="fa-solid fa-envelope" /> hsk@lifa.dk</p>
+            </div>
+        </div> 
+        <div class="portrait-container">
+            <img class="img" src="../../assets/img/allan_pb.jpg" alt="Allan">
+            <div class="image-overlay">
+                <p class="title">Forretningsspecialist, Partner</p>
+                <h3 class="name">Allan Larsen</h3>
+                <p class="contact-info"><font-awesome-icon icon="fa-solid fa-phone" /> +45 63 13 68 89</p>
+                <p class="contact-info"><font-awesome-icon icon="fa-solid fa-envelope" /> ala@lifa.dk</p>
+            </div>
+        </div> 
+        
+    </section>
+</article>
+
+
 </template>
 
 <style lang="scss">
-
+    @import "../contact/LoisContact.scss";
 </style>
